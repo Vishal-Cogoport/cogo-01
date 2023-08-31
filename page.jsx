56703b4b49e15567603dc28523d691535b8a20bc
Vishal Yadav <143420531+Vishal-Cogoport@users.noreply.github.com>
2023-08-31 16:16:08 +0530

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const teampage = () => {
+  return (
+    <div>
+        <h1>Hello Team</h1>
+        <p>
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+        </p>
+    </div>
+  );
+};
+
+export default teampage;
